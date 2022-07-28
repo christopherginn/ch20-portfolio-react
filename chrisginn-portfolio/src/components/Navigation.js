536fd84095f6fcde7cca13202da900d3f86974b6
christopherginn <hey.im.chris@live.com>
2022-07-28 18:33:40 -0400

    added bootstrap for styling

--- chrisginn-portfolio/src/components/Navigation.js
+++ chrisginn-portfolio/src/components/Navigation.js
@@ -2,6 +2,11 @@ import React from 'react';
 
 function Navigation({ currentPage, handlePageChange }) {
     return (
+        <div className="navbar navbar-expand-lg bg-light">
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+
         <ul>
             <li>
                 <a
@@ -36,6 +41,7 @@ function Navigation({ currentPage, handlePageChange }) {
                 </a>
             </li>
         </ul>
+        </div>
     )
 }
 
